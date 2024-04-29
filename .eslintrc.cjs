13a7d3034c3ed48d875efe1a031cd57955691859
Víctor <victor.af1104@gmail.com>
2024-04-29 23:17:10 +0200

    Add prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
