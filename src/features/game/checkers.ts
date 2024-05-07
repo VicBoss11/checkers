65dbde66a191a920943776d2b78dd3c71724fa12
Víctor <victor.af1104@gmail.com>
2024-05-07 21:07:44 +0200

    Add enhancements to calculate possible moves for a square with a piece

--- src/features/game/checkers.ts
+++ src/features/game/checkers.ts
@@ -1,134 +1,161 @@
 import { SQUARES_PER_SIDE } from './helpers/constants';
 import { Set } from './models/enums';
 import { Piece, Position, Square, TakenSquare } from './models/interfaces';
-import { Checkerboard } from './models/types';
+import { Checkerboard, MovePath } from './models/types';
 
-export function getPossibleMoves(
-  selectedSquare: Square,
+export function move(
+  from: TakenSquare,
+  to: Square,
   checkerboard: Checkerboard
-): Square[][] {
-  const possibleMoves: Square[][] = [];
-  const piece = selectedSquare.takenBy;
-
-  // If the square doesn't contain a piece, there are no possible moves
-  if (!piece) {
-    return possibleMoves;
-  }
-
-  const isLightSet = piece.set === Set.Light;
-  const { rowIndex: i, columnIndex: j } = selectedSquare.position;
-
-  // TODO: Uncomment the following lines if backward moves are allowed
-  // Check forward and backward moves
-  // const forwardRow = isLightSet ? i - 1 : i + 1;
-  // const backwardRow = isLightSet ? i + 1 : i - 1;
-
-  // Check forward moves
-  const forwardRow = isLightSet ? i - 1 : i + 1;
-  // Check diagonal moves
-  const leftColumn = isLightSet ? j - 1 : j + 1;
-  const rightColumn = isLightSet ? j + 1 : j - 1;
-
-  addValidMoves(
-    { rowIndex: forwardRow, columnIndex: leftColumn },
-    selectedSquare as TakenSquare,
-    checkerboard,
-    [],
-    possibleMoves
+): Checkerboard {
+  const updatedCheckerboard = checkerboard.map((row) =>
+    row.map((square) => ({ ...square }))
   );
 
-  addValidMoves(
-    { rowIndex: forwardRow, columnIndex: rightColumn },
-    selectedSquare as TakenSquare,
-    checkerboard,
-    [],
-    possibleMoves
-  );
+  const { rowIndex: fromRowIndex, columnIndex: fromColumnIndex } =
+    from.position;
 
-  // TODO: Uncomment the following lines if backward moves are allowed
-  // if (rule.backwards) {
-  //   addValidMoves(
-  //     { rowIndex: backwardRow, columnIndex: leftColumn },
-  //     selectedSquare as SquareTaken,
-  //     checkerboard,
-  //     [],
-  //     possibleMoves
-  //   );
+  const { rowIndex: toRowIndex, columnIndex: toColumnIndex } = to.position;
 
-  //   addValidMoves(
-  //     { rowIndex: backwardRow, columnIndex: rightColumn },
-  //     selectedSquare as SquareTaken,
-  //     checkerboard,
-  //     [],
-  //     possibleMoves
-  //   );
-  // }
+  const piece: Piece = {
+    ...updatedCheckerboard[fromRowIndex][fromColumnIndex].takenBy!,
+  };
 
-  return possibleMoves;
+  updatedCheckerboard[fromRowIndex][fromColumnIndex].takenBy = null;
+  updatedCheckerboard[toRowIndex][toColumnIndex].takenBy = piece;
+
+  return updatedCheckerboard;
 }
 
-// Recursive function to obtain all possible valid moves of a square
-function addValidMoves(
-  targetPosition: Position, // The position to check
-  selectedSquare: TakenSquare, // The current selected square
-  checkerboard: Checkerboard,
-  currentPathMoves: Square[], // An array with all the moves of the current path
-  possibleMoves: Square[][] // An array that stores all move paths
-) {
-  const { rowIndex: i, columnIndex: j } = targetPosition;
-  const isWithinCheckerboardBounds =
-    i >= 0 && i < SQUARES_PER_SIDE && j >= 0 && j < SQUARES_PER_SIDE;
-
-  if (!isWithinCheckerboardBounds) {
-    return;
-  }
-
-  const targetSquare = checkerboard[i][j];
-
-  if (!targetSquare.takenBy) {
-    const newPath = [...currentPathMoves, targetSquare];
-
-    possibleMoves.push(newPath);
-
-    return;
-  }
-
-  if (targetSquare.takenBy.set !== selectedSquare.takenBy.set) {
-    const newPath = [...currentPathMoves, targetSquare];
-
-    possibleMoves.push(newPath);
-
-    const isLightSet = selectedSquare.takenBy.set === Set.Light;
-
-    const forwardRow = isLightSet ? i - 1 : i + 1;
-    const leftColumn = isLightSet ? j - 1 : j + 1;
-    const rightColumn = isLightSet ? j + 1 : j - 1;
-
-    addValidMoves(
-      { rowIndex: forwardRow, columnIndex: leftColumn },
-      targetSquare as TakenSquare,
-      checkerboard,
-      newPath,
-      possibleMoves
-    );
-
-    addValidMoves(
-      { rowIndex: forwardRow, columnIndex: rightColumn },
-      targetSquare as TakenSquare,
-      checkerboard,
-      newPath,
-      possibleMoves
-    );
-  }
-}
-
-export function checkIsMovablePiece(
+export function isPieceMovable(
   piece: Piece,
-  possibleMoves: Square[][],
+  possibleMoves: MovePath[],
   turn: Set
 ): boolean {
   return (
     turn === piece.set && // The turn corresponds to the color of the piece
     possibleMoves.length > 0 // There are possible moves
   );
+}
+
+export function getPossibleMoves(
+  square: TakenSquare,
+  checkerboard: Checkerboard,
+  turn: Set
+): MovePath[] {
+  const possibleMoves: MovePath[] = [];
+
+  if (square.takenBy.set !== turn) {
+    return possibleMoves;
+  }
+
+  addValidMoves(square, turn, checkerboard, possibleMoves);
+
+  return possibleMoves;
+}
+
+function addValidMoves(
+  square: Square,
+  set: Set,
+  checkerboard: Checkerboard,
+  possibleMoves: MovePath[],
+  currentMovePath: MovePath = [],
+  isCaptureMove = false
+): void {
+  const { forwardRow, columnOffsets } = calculateMoveOffsets(
+    set,
+    square.position
+  );
+
+  for (const columnOffset of columnOffsets) {
+    if (isWithinCheckerboardBounds(forwardRow, columnOffset)) {
+      const targetSquare = checkerboard[forwardRow][columnOffset];
+
+      addValidMovesOf(
+        { currentSquare: square, targetSquare },
+        set,
+        checkerboard,
+        possibleMoves,
+        currentMovePath,
+        isCaptureMove
+      );
+    }
+  }
+}
+
+function addValidMovesOf(
+  {
+    currentSquare,
+    targetSquare,
+  }: { currentSquare: Square; targetSquare: Square },
+  set: Set,
+  checkerboard: Checkerboard,
+  possibleMoves: MovePath[],
+  currentMovePath: MovePath = [],
+  isCaptureMove = false
+): void {
+  if (!targetSquare.takenBy && !isCaptureMove) {
+    const newMovePath: MovePath = [...currentMovePath, targetSquare];
+
+    possibleMoves.push(newMovePath);
+
+    return;
+  }
+
+  if (targetSquare.takenBy?.set === set) {
+    return;
+  }
+
+  const { rowIndex: oppositeRowIndex, columnIndex: oppositeColumnIndex } =
+    calculateDiagonalOffset(currentSquare.position, targetSquare.position);
+
+  if (
+    isWithinCheckerboardBounds(oppositeRowIndex, oppositeColumnIndex) &&
+    !checkerboard[oppositeRowIndex][oppositeColumnIndex].takenBy
+  ) {
+    const newTargetSquare = checkerboard[oppositeRowIndex][oppositeColumnIndex];
+    const newMovePath: MovePath = [...currentMovePath, newTargetSquare];
+    const captureMode = true;
+
+    possibleMoves.push(newMovePath);
+
+    addValidMoves(
+      newTargetSquare,
+      set,
+      checkerboard,
+      possibleMoves,
+      newMovePath,
+      captureMode
+    );
+  }
+}
+
+function isWithinCheckerboardBounds(i: number, j: number): boolean {
+  return i >= 0 && i < SQUARES_PER_SIDE && j >= 0 && j < SQUARES_PER_SIDE;
+}
+
+function calculateMoveOffsets(
+  turn: Set,
+  { rowIndex, columnIndex }: Position
+): { forwardRow: number; columnOffsets: [number, number] } {
+  const isLightSet = turn === Set.Light;
+
+  const forwardRow = isLightSet ? rowIndex - 1 : rowIndex + 1;
+  const leftColumn = isLightSet ? columnIndex - 1 : columnIndex + 1;
+  const rightColumn = isLightSet ? columnIndex + 1 : columnIndex - 1;
+
+  return { forwardRow, columnOffsets: [leftColumn, rightColumn] };
+}
+
+function calculateDiagonalOffset(
+  { rowIndex: startRowIndex, columnIndex: startColumnIndex }: Position,
+  { rowIndex: endRowIndex, columnIndex: endColumnIndex }: Position
+): Position {
+  const deltaRow = endRowIndex - startRowIndex;
+  const deltaColumn = endColumnIndex - startColumnIndex;
+
+  const oppositeRowIndex = endRowIndex + deltaRow;
+  const oppositeColumnIndex = endColumnIndex + deltaColumn;
+
+  return { rowIndex: oppositeRowIndex, columnIndex: oppositeColumnIndex };
 }
