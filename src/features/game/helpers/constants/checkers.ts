e92fd60f48e45fb87df3beee016d05e6082356d4
Víctor <victor.af1104@gmail.com>
2024-05-20 20:18:59 +0200

    Update constants and add default values

---
+++ src/features/game/helpers/constants/checkers.ts
@@ -0,0 +1,5 @@
+import { DEFAULT_PIECES_LOCATION_TEMPLATE } from '.';
+
+export const SQUARES_PER_SIDE = DEFAULT_PIECES_LOCATION_TEMPLATE.length;
+export const FIRST_ROW_INDEX = 0;
+export const LAST_ROW_INDEX = SQUARES_PER_SIDE - 1;
--- src/features/game/helpers/constants/index.ts
+++ src/features/game/helpers/constants/index.ts
@@ -1,3 +1,44 @@
+import { GameMode, Set } from '../../models/enums';
+import { Settings, RuleSet } from '../../models/interfaces/game-settings';
+import { Player } from '../../models/interfaces/game-state';
+
+export const DEFAULT_SETTINGS: Settings = {
+  gameMode: GameMode.Local,
+  // TODO: Set to false
+  showCompletePaths: true,
+  drawRequestEnabled: true,
+  showCheckerboardCoordinates: true,
+  deepMode: false,
+};
+
+export const DEFAULT_RULES: RuleSet = {
+  captureRequired: false,
+  bestCapturePathRequired: false,
+  blowEnabled: false,
+  kingSingleStepEnabled: false,
+  timeLimitEnabled: false,
+  timeLimit: 300,
+  movesWithoutCaptureLimitEnabled: false,
+  movesWithoutCaptureLimit: 10,
+};
+
+export const DEFAULT_PLAYERS: Player[] = [
+  {
+    name: 'Player 1',
+    set: Set.Light,
+    isComputer: false,
+    elapsedTime: 0,
+    remainingTime: DEFAULT_RULES.timeLimit,
+  },
+  {
+    name: 'Player 2',
+    set: Set.Dark,
+    isComputer: false,
+    elapsedTime: 0,
+    remainingTime: DEFAULT_RULES.timeLimit,
+  },
+];
+
 export const DEFAULT_PIECES_LOCATION_TEMPLATE = [
   [0, null, 0, null, 0, null, 0, null],
   [null, 0, null, 0, null, 0, null, 0],
@@ -9,4 +50,12 @@ export const DEFAULT_PIECES_LOCATION_TEMPLATE = [
   [null, 1, null, 1, null, 1, null, 1],
 ];
 
-export const SQUARES_PER_SIDE = DEFAULT_PIECES_LOCATION_TEMPLATE.length;
+export const DEFAULT_REMAINIG_LIGHT_PIECES =
+  DEFAULT_PIECES_LOCATION_TEMPLATE.flat().filter(
+    (piece) => piece === Set.Light
+  ).length;
+
+export const DEFAULT_REMAINIG_DARK_PIECES =
+  DEFAULT_PIECES_LOCATION_TEMPLATE.flat().filter(
+    (piece) => piece === Set.Dark
+  ).length;
