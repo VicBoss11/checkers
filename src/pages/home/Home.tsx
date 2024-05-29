fdf59b15e06a9369ecda62e1f5b595fbbdd451ae
Víctor <victor.af1104@gmail.com>
2024-05-29 22:11:24 +0200

    Add Home page component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+import Layout from '@features/ui/components/layout/Layout';
+
+function Home(): ReactElement {
+  return (
+    <Layout>
+      <h1>Home</h1>
+    </Layout>
+  );
+}
+
+export default Home;
