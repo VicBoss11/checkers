f5d43285c8707207431d65cc19c038dd10160e11
Víctor <victor.af1104@gmail.com>
2024-05-29 22:12:53 +0200

    Add basic routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import Checkers from './features/game/components/checkers/Checkers';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Play from './pages/play/Play';
 import './App.scss';
 
 function App() {
-  return <Checkers />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/play" element={<Play />} />
+    </Routes>
+  );
 }
 
 export default App;
