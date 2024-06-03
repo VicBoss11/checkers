1e4a929d6ed721cf1793f1a34a261632354982bf
Víctor <victor.af1104@gmail.com>
2024-06-03 17:43:48 +0200

    Refactor Play page component structure and styles

--- src/pages/play/Play.tsx
+++ src/pages/play/Play.tsx
@@ -3,6 +3,7 @@ import { CheckersProvider } from '@features/game/context/checkers-context';
 import TurnInfo from '@features/game/components/turn-info/TurnInfo';
 import Checkers from '@features/game/components/checkers/Checkers';
 import GameSatus from '@features/game/components/game-status/GameStatus';
+import MoveLog from '@features/game/components/move-log/MoveLog';
 import Layout from '@features/ui/components/layout/Layout';
 import './Play.scss';
 
@@ -11,12 +12,19 @@ function Play(): ReactElement {
     <Layout>
       <CheckersProvider>
         <section className="play">
-          <section className="game-container">
-            <TurnInfo></TurnInfo>
+          <section className="game">
+            <aside className="turn-info-container">
+              <TurnInfo></TurnInfo>
+            </aside>
 
-            <GameSatus></GameSatus>
+            <aside className="game-info">
+              <GameSatus></GameSatus>
+              <MoveLog></MoveLog>
+            </aside>
 
-            <Checkers></Checkers>
+            <section className="checkers-container">
+              <Checkers></Checkers>
+            </section>
           </section>
         </section>
       </CheckersProvider>
